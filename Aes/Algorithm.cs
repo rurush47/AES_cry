5cefc04a1f2a3eb7ea45e828221f5283581901f6
RuRush47 <olekrurarz@gmail.com>
2017-01-05 17:47:38 +0100

    Add Algorithm class

---
+++ Aes/Algorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aes
+{
+    class Algorithm
+    {
+        public static string Encrypt(string message, Key key)
+        {
+            Console.WriteLine(key.ToString());
+            //TODO
+            return null;
+        }
+
+        public static string Decrypt(string cypher, Key key)
+        {
+            //TODO
+            return null;
+        }
+    }
+}
